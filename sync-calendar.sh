14479cceeb15337735c98a2faad6eb7a61acc1d1
guowei <guowei01@mobike.com>
2018-01-27 01:01:00 +0800

    x

---
+++ sync-calendar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo 'start'
+pwd
+cd /root/guowei/sumrise.github.io
+wget https://calendar.google.com/calendar/ical/7je3t02hd7r2oei4c0osmgs8fo%40group.calendar.google.com/private-ee632e40105552ff877304e2af938a72/basic.ics
+
+DIFF="$(diff basic.ics basic.ics.1 |grep 'END')"
+if [ "$DIFF" != "" ]; then
+    echo "DIFFERENT"
+    echo "$DIFF"
+    rm -rf basic.ics
+    mv basic.ics.1 basic.ics
+
+    git add -A
+    git commit -m 'sync calendar '
+    git push origin master
+
+fi
+rm -rf basic.ics.1
+echo "end"
